da30d6c1b5aa62972749169b2ceb483a1115f2c9
Ajay-driod-cmd <ajay.t5102001@gmail.com>
2022-03-19 12:55:22 +0530

    Scrimba course

---
+++ React-playground/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import './test'
+import Custom from './Custom';
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+         React Playground
+        </p>
+       <Custom />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ React-playground/src/test.js
@@ -0,0 +1,17 @@
+function test(){
+return(
+    <div className="test">
+        <div className="prop">
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+                <li>4</li>
+                <li>5</li>
+            </ul>
+        </div>
+    </div>
+)
+}
+
+export default test;
